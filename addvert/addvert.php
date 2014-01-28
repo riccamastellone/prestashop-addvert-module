160f05651f38902b7c901e301e67ac3a29c22c11
Riccardo Mastellone <riccardo.mastellone@gmail.com>
2014-01-28 16:17:16 +0100

    right image

--- addvert/addvert.php
+++ addvert/addvert.php
@@ -166,7 +166,7 @@ class Addvert extends Module
 
             $image = Product::getCover($product->id);
             if (isset($image['id_image'])) {
-                $metas[] = array('property' => 'og:image', 'content' => $this->context->link->getImageLink($product->link_rewrite, $image['id_image'], 'home_default'));
+                $metas[] = array('property' => 'og:image', 'content' => $this->context->link->getImageLink($product->link_rewrite, $image['id_image']));
             }
 
             if ($categoryId = $this->getDefaultCategory($product)) {
